bb32ba3c0a946715b8e2982a5950248ef9db6fdb
Francesco Bronzino <wontoniii@gmail.com>
2022-04-21 12:13:34 +0200

    Added testing for bigger files

--- cfv/functions/sink.py
+++ cfv/functions/sink.py
@@ -8,6 +8,10 @@ class Sink(Function):
 
     '''
     Function.__init__(self)
+    self._first = True
+    self._stime = None
+    self._etime = None
+    self._received = 0
 
   async def push(self, id, msg):
     '''
@@ -15,4 +19,14 @@ class Sink(Function):
     :param port:
     :return:
     '''
-    logging.debug("Received frame at time {}. Discarding it".format(datetime.datetime.now().timestamp()))
+    rec_time = datetime.datetime.now()
+    logging.debug("Received frame at time {}. Discarding it".format(rec_time.timestamp()))
+    self._received += 1
+    if self._first:
+      self._stime = rec_time
+      self._first = False
+    else:
+      self._etime = rec_time
+      fps = self._received/((self._etime - self._stime).total_seconds())
+      logging.debug("Current fps: {}".format(fps))
+    
